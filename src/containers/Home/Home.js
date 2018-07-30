281e8a615c391c7a4213bbc852c81ca62ae1c9c2
Fergus <fergus.jingtai@gmail.com>
2018-07-30 11:36:42 +0800

    add opening layout

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -4,7 +4,18 @@ class Home extends Component {
   render() {
     return (
       <div id="app">
-        Home
+        <div className="opening">
+          <div className="star">
+            <div className="star__top" />
+            <div className="star__bottom" />
+          </div>
+          <div className="square" />
+          <div className="circle" />
+          <div className="text-block">
+            <div className="text-block__title">GEOMETRY PERSON</div>
+            <div className="text-block__desc">find your core personality type in shapes!</div>
+          </div>
+        </div>
       </div>
     );
   }
